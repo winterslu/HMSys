a614d5fd3d90760dc94a63c224c513f085408206
WintersLu <luke.s.winters@gmail.com>
2013-12-16 16:32:02 -0800

    Adding cursor paiting and GDT IDT initialization

--- tolset/helloos/bootpack.c
+++ tolset/helloos/bootpack.c
@@ -14,6 +14,9 @@ void colorBox(char *vram);
 void initialScreen(char *vram, int xsize, int ysize);
 void paintFont8(char *vram, int xsize, int x, int y, char c, char *font);
 void paintFonts8(char *vram, int xsize, int x, int y, char c, unsigned char *s);
+void initMouseCursor8(char *mouse, char bc);
+void putBlock8(char *vram, int vxsize, int pxsize,
+	int pysize, int px0, int py0, char *buf, int bxsize);
 
 #define COL8_000000		0
 #define COL8_FF0000		1
@@ -38,12 +41,33 @@ struct BOOTINFO {
 	char *vram;
 };
 
+struct SEGMENT_DESCRIPTOR {
+	short limit_low, base_low;
+	char base_mid, access_right;
+	char limit_high, base_high;
+};
+
+struct GATE_DESCRIPTOR {
+	short offset_low, selector;
+	char dw_count, access_right;
+	short offset_high;
+};
+
+void init_gdtidt(void);
+void set_segmdesc(struct SEGMENT_DESCRIPTOR *sd, unsigned int limit, int base, int ar);
+void set_gatedesc(struct GATE_DESCRIPTOR *gd, int offset, int selector, int ar);
+void load_gdtr(int limit, int addr);
+void load_idtr(int limit, int addr);
+
 void HariMain(void)
 {	
+	init_gdtidt();
 	init_palette();
 	
 	struct BOOTINFO *binfo = (struct BOOTINFO *) 0x0ff0;
 	extern char hankaku[4096];
+	char mcursor[256];
+	
 	// Flush screen with color
 	// int i;
 	// for(i = 0xa0000; i <= 0xaffff; i++){
@@ -59,6 +83,11 @@ void HariMain(void)
 	paintFonts8(binfo->vram, binfo->scrnx, 8, 8, COL8_FFFFFF, "ABC 123");
 	paintFonts8(binfo->vram, binfo->scrnx, 31, 31, COL8_000000, "Winters Lu");
 	paintFonts8(binfo->vram, binfo->scrnx, 30, 30, COL8_FFFFFF, "Winters Lu");
+	
+	int mx = (binfo->scrnx - 16) / 2;
+	int my = (binfo->scrny - 28 - 16) / 2;
+	initMouseCursor8(mcursor, COL8_008484);
+	putBlock8(binfo->vram, binfo->scrnx, 16, 16, mx, my, mcursor, 16);
 
 	for(;;){
 		io_hlt();
@@ -90,6 +119,56 @@ void paintFonts8(char *vram, int xsize, int x, int y, char c, unsigned char *s)
 	for(; *s != 0x00; s++){
 		paintFont8(vram, xsize, x, y, c, hankaku + *s * 16);
 		x += 8;
+	}
+	return;
+}
+
+void initMouseCursor8(char *mouse, char bc)
+{
+	static char cursor[16][16] = {
+		"**************..",
+		"*OOOOOOOOOOO*...",
+		"*OOOOOOOOOO*....",
+		"*OOOOOOOOO*.....",
+		"*OOOOOOOO*......",
+		"*OOOOOOO*.......",
+		"*OOOOOOO*.......",
+		"*OOOOOOOO*......",
+		"*OOOO**OOO*.....",
+		"*OOO*..*OOO*....",
+		"*OO*....*OOO*...",
+		"*O*......*OOO*..",
+		"**........*OOO*.",
+		"*..........*OOO*",
+		"............*OO*",
+		".............***"
+	};
+	int x, y;
+
+	for (y = 0; y < 16; y++) {
+		for (x = 0; x < 16; x++) {
+			if (cursor[y][x] == '*') {
+				mouse[y * 16 + x] = COL8_000000;
+			}
+			if (cursor[y][x] == 'O') {
+				mouse[y * 16 + x] = COL8_FFFFFF;
+			}
+			if (cursor[y][x] == '.') {
+				mouse[y * 16 + x] = bc;
+			}
+		}
+	}
+	return;
+}
+
+void putBlock8(char *vram, int vxsize, int pxsize,
+	int pysize, int px0, int py0, char *buf, int bxsize)
+{
+	int x, y;
+	for (y = 0; y < pysize; y++) {
+		for (x = 0; x < pxsize; x++) {
+			vram[(py0 + y) * vxsize + (px0 + x)] = buf[y * bxsize + x];
+		}
 	}
 	return;
 }
@@ -170,3 +249,51 @@ void boxfill8(unsigned char *vram, int xsize, unsigned char c, int x0, int y0, i
 	}
 	return;
 }
+
+
+void init_gdtidt(void)
+{
+	struct SEGMENT_DESCRIPTOR *gdt = (struct SEGMENT_DESCRIPTOR *) 0x00270000;
+	struct GATE_DESCRIPTOR    *idt = (struct GATE_DESCRIPTOR    *) 0x0026f800;
+	int i;
+
+	for (i = 0; i < 8192; i++) {
+		set_segmdesc(gdt + i, 0, 0, 0);
+	}
+	set_segmdesc(gdt + 1, 0xffffffff, 0x00000000, 0x4092);
+	set_segmdesc(gdt + 2, 0x0007ffff, 0x00280000, 0x409a);
+	load_gdtr(0xffff, 0x00270000);
+
+	for (i = 0; i < 256; i++) {
+		set_gatedesc(idt + i, 0, 0, 0);
+	}
+	load_idtr(0x7ff, 0x0026f800);
+
+	return;
+}
+
+void set_segmdesc(struct SEGMENT_DESCRIPTOR *sd, unsigned int limit, int base, int ar)
+{
+	if (limit > 0xfffff) {
+		ar |= 0x8000; /* G_bit = 1 */
+		limit /= 0x1000;
+	}
+	sd->limit_low    = limit & 0xffff;
+	sd->base_low     = base & 0xffff;
+	sd->base_mid     = (base >> 16) & 0xff;
+	sd->access_right = ar & 0xff;
+	sd->limit_high   = ((limit >> 16) & 0x0f) | ((ar >> 8) & 0xf0);
+	sd->base_high    = (base >> 24) & 0xff;
+	return;
+}
+
+void set_gatedesc(struct GATE_DESCRIPTOR *gd, int offset, int selector, int ar)
+{
+	gd->offset_low   = offset & 0xffff;
+	gd->selector     = selector;
+	gd->dw_count     = (ar >> 8) & 0xff;
+	gd->access_right = ar & 0xff;
+	gd->offset_high  = (offset >> 16) & 0xffff;
+	return;
+}
+
